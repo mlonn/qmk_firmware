d6bc60176b52c77d15cda95777d94ba4c7706116
Mikael Lönn <mikael@sendify.se>
2023-10-04 14:32:07 +0200

    update from origin

--- keyboards/crkbd/keymaps/mlonn/config.h
+++ keyboards/crkbd/keymaps/mlonn/config.h
@@ -33,7 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define LAYER_STATE_16BIT
-#define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
